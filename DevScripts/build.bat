152e2ee0b011c957654c3d4c3a4e328d3e7206bb
andrejp191 <apollak35@gmail.com>
2025-04-08 12:21:03 +0200

    ConsoleAppCpp project

--- DevScripts/build.bat
+++ DevScripts/build.bat
@@ -1,1 +1,11 @@
-@echo "Hello from Build"
+@echo "Starting Build"
+set "cur_dir=%CD%"
+
+cd /d m: m:\DevScripts\Build\VS_Tools\
+callvcvarsall.bat x64
+
+cd /d "%OLDDIR%"
+cd Projects\ConsoleApplicationCPP
+devenv ConsoleApplicationCPP.vcxproj
+
+@echo "Finishing Build"
