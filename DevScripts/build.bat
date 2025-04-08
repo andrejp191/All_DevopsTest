efa7894e154c8bc19083d753509a48c7c1c5d6a2
andrejp191 <apollak35@gmail.com>
2025-04-08 13:29:25 +0200

    Update build.bat

--- DevScripts/build.bat
+++ DevScripts/build.bat
@@ -1,10 +1,13 @@
+@echo on
 @echo "Starting Build"
 set "cur_dir=%CD%"
 
 cd /d m:\DevScripts\Build\VS_Tools
 call callvcvarsall.bat x64
 
+dir *.*
 cd /d "%cur_dir%"
+dir *.*
 cd /D ..\Projects\ConsoleApplicationCPP
 msbuild ConsoleApplicationCPP.vcxproj
 
