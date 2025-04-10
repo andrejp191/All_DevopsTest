04fdefa80b37fac8097894a988087f07a2ca5476
andrejp191 <apollak35@gmail.com>
2025-04-10 08:33:13 +0200

    Update build.bat

--- DevScripts/build.bat
+++ DevScripts/build.bat
@@ -5,9 +5,12 @@ set "cur_dir=%CD%"
 cd /d m:\DevScripts\Build\VS_Tools
 call callvcvarsall.bat x64
 
-cd /d "%cur_dir%"
-cd /D Projects\ConsoleApplicationCPP
+set path=%path%;C:\Users\apollak\AppData\Local\GitHubDesktop\app-3.4.18\resources\app\git\cmd
+cd /D D:\GitHub\All_DevopsTest
+git pull origin Devops
 
+cd Projects\ConsoleApplicationCPP
 msbuild ConsoleApplicationCPP.vcxproj
 
+cd /d "%cur_dir%"
 @echo "Finishing Build"
