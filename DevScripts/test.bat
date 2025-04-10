b74eaa17f028ba79586ecb46c8f7e64551951ce6
andrejp191 <apollak35@gmail.com>
2025-04-10 10:30:33 +0200

    group log lines

--- DevScripts/test.bat
+++ DevScripts/test.bat
@@ -1,2 +1,15 @@
 @echo "Test started"
+echo ::group::Testsuite 1
+echo ::warning::test suite warning
+echo ::endgroup::
+
+echo ::group::Testsuite 2
+
+echo ::endgroup::
+
 @echo "Test finished"
+
+
+echo ### Batch File Results > %GITHUB_STEP_SUMMARY%
+echo - Build completed successfully >> %GITHUB_STEP_SUMMARY%
+echo - Tests passed: 42 >> %GITHUB_STEP_SUMMARY%
